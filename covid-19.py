ef53cea85c288139bafbeab113aafccfe34cb1bb
anzacsf2010 <40782333+anzacsf2010@users.noreply.github.com>
2020-05-05 17:55:37 -0500

    Update covid-19.py

--- covid-19.py
+++ covid-19.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 #-------------------------------------------#
 import pandas as pd 
-import numpy as np  
-import matplotlib.pyplot as plt
+import numpy as np
 import requests
 import io
 from datetime import date, timedelta
